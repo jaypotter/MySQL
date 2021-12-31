4c0af049b8aaf269584e4b0271cca78309e5b4ba
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-31 14:24:04 -0500

    showTablesIn

--- Potter/MySQL/Connection/MySQLConnectionInterface.php
+++ Potter/MySQL/Connection/MySQLConnectionInterface.php
@@ -7,9 +7,9 @@ use Potter\Database\Connection\Remote\RemoteDatabaseConnectionInterface;
 interface MySQLConnectionInterface extends RemoteDatabaseConnectionInterface
 {
     public const SHOW_DATABASES = "SHOW DATABASES";
-    public const SHOW_TABLES = "SHOW TABLES";
+    public const SHOW_TABLES_IN = "SHOW TABLES IN";
 
     public function showDatabases(): array;
 
-    public function showTables(string $database): array;
+    public function showTablesIn(string $database): array;
 }
