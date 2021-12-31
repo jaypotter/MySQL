514ea8de7dab33efaa0ef817f12a888f3180dfe9
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-31 17:28:14 -0500

    Update MySQLConnectionInterface.php

--- Potter/MySQL/Connection/MySQLConnectionInterface.php
+++ Potter/MySQL/Connection/MySQLConnectionInterface.php
@@ -6,6 +6,7 @@ use Potter\Database\Connection\Remote\RemoteDatabaseConnectionInterface;
 
 interface MySQLConnectionInterface extends RemoteDatabaseConnectionInterface
 {
+    public const CREATE_DATABASE = "CREATE DATABASE";
     public const SHOW_DATABASES = "SHOW DATABASES";
     public const SHOW_TABLES_IN = "SHOW TABLES IN";
 
