001562e540c0ad437ffb781b7c0bb1de4eecfd6b
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-31 13:16:29 -0500

    Create MySQLiTrait.php

---
+++ Potter/MySQLi/MySQLiTrait.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Potter\MySQLi;
+
+use Potter\MySQLi\Native\NativeMySQLi;
+
+trait MySQLiTrait
+{
+    final public function connect(): void
+    {
+        $this->setObject(
+            new NativeMySQLi(
+                hostname: $this->getHost(),
+                username: $this->getUser(),
+                password: $this->getPass(),
+                port: $this->getPort()
+            )
+        );
+    }
+
+    abstract public function getHost(): string;
+
+    abstract public function getPass(): string;
+
+    abstract public function getPort(): int;
+
+    abstract public function getUser(): string;
+}
