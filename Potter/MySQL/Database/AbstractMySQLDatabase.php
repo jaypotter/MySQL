fa449d2686a9874225906b5f70da13941d81a846
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-31 14:24:31 -0500

    Update AbstractMySQLDatabase.php

--- Potter/MySQL/Database/AbstractMySQLDatabase.php
+++ Potter/MySQL/Database/AbstractMySQLDatabase.php
@@ -2,9 +2,14 @@
 
 namespace Potter\MySQL\Database;
 
-use Potter\Database\AbstractDatabase;
+use Potter\Database\{
+    AbstractDatabase,
+    DatabaseTrait
+};
 
 abstract class AbstractMySQLDatabase extends AbstractDatabase implements MySQLDatabaseInterface
 {
+    use DatabaseTrait;
+    
     abstract public function showTables(): array;
 }
