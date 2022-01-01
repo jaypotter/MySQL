05076449da5f3129baeb6c21791f22aac78799ba
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2022-01-01 12:39:39 -0500

    Update MySQLConnectionInterface.php

--- Potter/MySQL/Connection/MySQLConnectionInterface.php
+++ Potter/MySQL/Connection/MySQLConnectionInterface.php
@@ -6,12 +6,21 @@ use Potter\Database\Connection\Remote\RemoteDatabaseConnectionInterface;
 
 interface MySQLConnectionInterface extends RemoteDatabaseConnectionInterface
 {
-    public const CREATE_DATABASE = "CREATE DATABASE";
-    public const SHOW_CHARACTER_SET = "SHOW CHARACTER SET";
-    public const SHOW_DATABASES = "SHOW DATABASES";
-    public const SHOW_TABLES_IN = "SHOW TABLES IN";
+    public const CREATE_DATABASE = 'CREATE DATABASE';
+    public const SHOW_CHARACTER_SET = 'SHOW CHARACTER SET';
+    public const SHOW_COLLATION = 'SHOW COLLATION';
+    public const SHOW_DATABASES = 'SHOW DATABASES';
+    public const SHOW_TABLES_IN = 'SHOW TABLES IN';
+
+    public function characterSetExists(string $characterSet): bool;
+    
+    public function getCharacterSet(): array;
+
+    //public function getCollation(): array;
 
     public function showCharacterSet(): array;
+
+    //public function showCollation(): array;
 
     public function showDatabases(): array;
 
