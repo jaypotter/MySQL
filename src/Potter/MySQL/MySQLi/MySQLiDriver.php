474f6ce11095f87ebceba7dc70aa89bf738c39dc
Jay Potter <j@ypotter.ca>
2024-09-01 21:55:34 -0400

    Update MySQLiDriver.php

--- src/Potter/MySQL/MySQLi/MySQLiDriver.php
+++ src/Potter/MySQL/MySQLi/MySQLiDriver.php
@@ -23,7 +23,7 @@ final class MySQLiDriver extends AbstractMySQLDriver
         return new MySQLiStatement($query, $handle);
     }
     
-    public function createDatabase(string $name, string $charset = self::CHARSET, string $collation = self::COLLATION): void
+    public function createDatabase(object $handle, string $name, string $charset = self::CHARSET, string $collation = self::COLLATION): void
     {
         $this->validateNewDatabase($name, $charset, $collation);
         ($this->prepare("CREATE DATABASE $database DEFAULT CHARACTER SET = '$charset' DEFAULT COLLATE = '$collate';", $handle))->execute();
@@ -50,7 +50,7 @@ final class MySQLiDriver extends AbstractMySQLDriver
         return $statement->getResult();
     }
     
-    public function use(string $database, object $handle): void
+    public function use(object $handle, string $database): void
     {
         ($this->prepare("USE $database;", $handle))->execute();
     }
