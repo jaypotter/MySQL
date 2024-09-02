0dba23f5549bc2c31a064960da227bf93c3ef2bc
Jay Potter <j@ypotter.ca>
2024-09-01 22:07:36 -0400

    Update MySQLDriverInterface.php

--- src/Potter/MySQL/Driver/MySQLDriverInterface.php
+++ src/Potter/MySQL/Driver/MySQLDriverInterface.php
@@ -12,7 +12,7 @@ interface MySQLDriverInterface extends DatabaseDriverInterface
     public const string DEFAULT_CHARSET = 'utf8mb4';
     public const string DEFAULT_COLLATION = 'utf8_unicode_ci';
     
-    public function createDatabase(object $handle, string $name, string $charset = self::DEFAULT_CHARSET, string $collation = self::DEFAULT_COLLATION): void;
+    public function createDatabase(object $handle, string $database, string $charset = self::DEFAULT_CHARSET, string $collation = self::DEFAULT_COLLATION): void;
     public function selectDatabase(object $handle): ResultInterface;
     public function showDatabases(object $handle): ResultInterface;
     public function use(object $handle, string $database): void;
