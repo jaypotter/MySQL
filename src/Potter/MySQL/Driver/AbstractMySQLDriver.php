1a334256da19b7fb978fd90a434c9d438a4c556f
Jay Potter <j@ypotter.ca>
2024-09-02 21:24:46 -0400

    Update AbstractMySQLDriver.php

--- src/Potter/MySQL/Driver/AbstractMySQLDriver.php
+++ src/Potter/MySQL/Driver/AbstractMySQLDriver.php
@@ -12,8 +12,9 @@ use Potter\Database\{
 abstract class AbstractMySQLDriver extends AbstractDatabaseDriver implements MySQLDriverInterface
 {
     abstract public function createDatabase(object $handle, string $database, string $charset = self::DEFAULT_CHARSET, string $collation = self::DEFAULT_COLLATION): void;
-    abstract public function createTable(string $table, ColumnInterface ...$columns): void;
     abstract public function dropDatabase(object $handle, string $database): void;
+    abstract public function createTable(object $handle, tring $table, ColumnInterface ...$columns): void;
+    abstract public function dropTable(object $handle, string $database): void;
     abstract public function selectDatabase(object $handle): ResultInterface;
     abstract public function showDatabases(object $handle): ResultInterface;
     abstract public function showTables(object $handle): ResultInterface;
