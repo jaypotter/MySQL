f67e88c98b231f402b5d0883dde0b3338998324b
Jay Potter <j@ypotter.ca>
2024-09-09 19:18:14 -0400

    Update MySQLiStatement.php

--- src/Potter/MySQL/MySQLi/MySQLiStatement.php
+++ src/Potter/MySQL/MySQLi/MySQLiStatement.php
@@ -24,7 +24,7 @@ final class MySQLiStatement extends AbstractStatement
         return $this->query;
     }
     
-    public function execute(array $vars = []): void
+    public function execute(mixed ...$vars): void
     {
         $this->statement->execute($vars);
     }
